0d24d379ea3627b286a2c8b469658c9bb99b2494
Gopal-Sable <sablegopal123@gmail.com>
2022-03-26 11:36:34 +0530

    data saved to database but not validated

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="https://kit.fontawesome.com/467cb691e7.js" crossorigin="anonymous"></script>
   <title>e-Learn</title>
 </head>
@@ -19,6 +19,7 @@
 
   <!-- navigation start -->
  <?php
+ include("DB/dbConnection.php");
  include("Nav.php");
  ?>
   <!-- navigation end -->
