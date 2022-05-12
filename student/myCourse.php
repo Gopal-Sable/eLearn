2b9ef64d3b89c16aa645607ab5e857a95f260633
Gopal-Sable <sablegopal123@gmail.com>
2022-05-12 17:07:47 +0530

    cleaning project

--- student/myCourse.php
+++ student/myCourse.php
@@ -10,17 +10,34 @@
         header("location:../index.php");
     }
 ?>
-<div class="container mt-5ml-4">
+<div class="col-sm-9 container mt-5 ml-4">
     <div class="row">
         <div class="jumbotron">
             <h4 class="text-center">All Course</h4>
             <?php
             if (isset($mail)) {
-                $q="select o.txn_id,c.id,c.course_id,c.duration,c.course_desc,c.course_img,c.Author,c.original_price,c.price from payment AS o JOIN course AS c ON c.id=o.course_id where o.stud_id='$mail'";
+                $q="SELECT o.txn_id,c.id,c.course_name,c.duration,c.course_desc,c.course_img,c.Author,c.original_price,c.price FROM payment AS o JOIN course AS c ON c.id=o.course_id where o.stud_id='$mail'";
                 $result=$con->query($sql);
                 if ($result->num_rows>0) {
-                    while($row=$result->fetch_assoc()){
-                        
+                    while($row=$result->fetch_assoc()){?>
+                        <div class="bg-light mb-3">
+                            <h5 class="car-header"><?php echo $row['course_name']?></h5>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <img src="image/upload/<?php echo $row['course_img']?>" class="card-img-top mt-4" alt="img">
+                                </div>
+                                <div class="col-sm-6 mb-3">
+                                    <div class="card-body">
+                                        <p class="card-title"><?php echo $row['course_desc']?></p>
+                                        <small class="card-text">Duration:<?php echo $row['duration']?></small><br>
+                                        <small class="card-text">Instructor:<?php echo $row['Author']?></small><br>
+                                        <a href="watchcourse.php?cid=<?php echo$row["course_id"]?>" class="btn btn-primary mt-5 float-right">Start Learning</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php
                     }
                 }
             }
@@ -28,3 +45,6 @@
         </div>
     </div>
 </div>
+<?php
+include_once("./header/footer.php")
+?>
